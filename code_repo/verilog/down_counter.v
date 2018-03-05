eb5a138c76cc9b9131000fd46a95e32e9fcb5502
sriram mano <sriramkmanoharan@gmail.com>
2018-03-04 19:29:41 -0700

    [verilog] Change to 8-bit counter

--- code_repo/verilog/down_counter.v
+++ code_repo/verilog/down_counter.v
@@ -4,27 +4,29 @@ module down_counter(
      input nrst,
      input load,
      input divide_by_2,
-  input      [3:0] count_in,
-  output reg [3:0] count_out,
-  	 output zero
+     
+     input      [7:0] preload_count,
+     output reg [7:0] count_out,
+  	 
+     output zero
 );
 
-  always@(posedge clk or negedge nrst)
- begin
-   if(nrst & !load)
+ always@(posedge clk or negedge nrst)
+  begin
+    if(nrst & !load)
       //When reset in high since this is a down counter set all bits to high,
       //so we can start counting down after reset is deasserted
-  		count_out <= 8'b1111_1111;
- 	else if(load)
+  		count_out <= 8'hFF;
+ 	 else if(load)
       //This is to load the counter with any arbitrary value and count down from that
-  		count_out <= count_in;
- 	else if(divide_by_2)
-      //As specified in the question we can divide the count by 2 by using the right shift operator ">>"
+  		count_out <= preload_count;
+ 	 else if(divide_by_2)
       //when ever this signal is high that count_out will be divided by 2
   		count_out <= count_out >> 1;
-   else if (!zero)
+     else if (!zero)
   		count_out <= count_out - 1'b1;
- end
+  end
 
          assign zero = count_out == 0 ;
+
 endmodule
